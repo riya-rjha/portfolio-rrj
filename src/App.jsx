3c4c2af14485d119bfde8abbb3e2a8daccaa2772
RRJ <riyaranjanjha2006@gmail.com>
2024-06-26 22:02:59 +0530

    Removing Theme

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,13 @@ import Footer from "./Components/Footer";
 import AboutUs from "./Components/About";
 
 const App = () => (
-  <div className="font-sans bg-gray-100">
+  <div id="top" className="font-sans bg-gray-100">
+    <a href="#top">
+      <img
+        src="https://cdn-icons-png.freepik.com/256/15992/15992789.png?ga=GA1.1.224769648.1717002388&semt=ais_hybrid"
+        className="fixed w-16 bottom-5 right-5 z-50"
+      />
+    </a>
     <Header />
     <Main />
     <AboutUs />
