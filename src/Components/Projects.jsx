dfa6a52bb24585227ed9a943ce26c70b252a1d90
RRJ <riyaranjanjha2006@gmail.com>
2024-07-18 00:24:57 +0530

    Updated Skills section

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -106,7 +106,7 @@ const Projects = () => {
             className="bg-white text-gray-800 shadow-lg p-6 rounded-lg flex flex-col md:flex-row items-center transform transition duration-500 hover:scale-105"
           >
             <div className="md:w-1/2 md:pr-6">
-              <h3 className="text-2xl font-bold mb-4 uppercase">{project.name}</h3>
+              <h3 className="text-3xl font-bold mb-4 uppercase">{project.name}</h3>
               <p className="mb-4 text-lg">
                 <span className="font-bold">Tech Stack: </span>{project.tech}
               </p>
