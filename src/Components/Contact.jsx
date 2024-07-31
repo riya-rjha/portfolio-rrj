cec1913d8d2481cd562b894a44d0a9849ec17a2f
RRJ <riyaranjanjha2006@gmail.com>
2024-08-01 02:36:38 +0530

    Animations all done

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const location = useLocation().pathname;
 
-export default Contact
+  return (
+    <div
+      className={` ${location === '/' ? "mt-16" : "mt-0"}  min-h-screen bg-[url('https://img.freepik.com/free-vector/monochromatic-hand-painted-background-with-drawn-nature-elements_52683-63007.jpg?ga=GA1.1.448448890.1721050418&semt=ais_hybrid')] dark:bg-[url('https://img.freepik.com/premium-photo/dark-background-with-silver-blue-pattern-with-words-blue-moon-it_1290686-20842.jpg?ga=GA1.1.448448890.1721050418&semt=ais_hybrid')] bg-cover bg-no-repeat flex flex-col items-center justify-center p-6`}
+    >
+      <div className="text-center mb-8">
+        <h1 className="text-4xl font-bold text-purple-800 mb-4 dark:text-purple-400">
+          Why Don't You Come Say Hi?
+        </h1>
+        <p className="text-lg text-gray-800 dark:text-white">
+          Any feedback or suggestions, please feel free to drop here a message!
+          I'd love to hear from you.
+        </p>
+      </div>
+      <form className="bg-purple-200 dark:bg-white text-black rounded-lg shadow-lg p-8 max-w-lg w-full">
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-md font-semibold mb-2">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Your Name"
+            required
+            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-md font-semibold mb-2">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your Email"
+            required
+            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="feedback"
+            className="block text-md font-semibold mb-2"
+          >
+            Feedback
+          </label>
+          <textarea
+            id="feedback"
+            name="feedback"
+            placeholder="Your Message"
+            rows="5"
+            required
+            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 h-40 resize-none"
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="w-full py-3 bg-purple-600 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 transition duration-300"
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -11,7 +11,7 @@ const Skills = () => {
 
   return (
     <div className="p-4 dark:bg-[url('https://img.freepik.com/premium-photo/dark-background-with-silver-blue-pattern-with-words-blue-moon-it_1290686-20842.jpg?ga=GA1.1.448448890.1721050418&semt=ais_hybrid')] bg-no-repeat bg-cover mt-16  bg-[url('https://img.freepik.com/free-vector/monochromatic-hand-painted-background-with-drawn-nature-elements_52683-63007.jpg?ga=GA1.1.448448890.1721050418&semt=ais_hybrid')]">
-      <h1 className="text-4xl font-bold light:mt-8 dark:mt-3 dark:mb-5 uppercase all-headings dark:text-white text-center ">
+      <h1 className="text-4xl font-bold light:mt-8 my-5 uppercase all-headings dark:text-white text-center ">
         SKILLS
       </h1>
 
@@ -21,74 +21,74 @@ const Skills = () => {
         </h2>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5">
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://cdn-icons-png.freepik.com/256/5968/5968292.png?ga=GA1.1.448448890.1721050418&semt=ais_hybrid"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Javascript
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://cdn-icons-png.freepik.com/256/5968/5968381.png?ga=GA1.1.448448890.1721050418&semt=ais_hybrid"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Typescript
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://cdn-icons-png.freepik.com/256/919/919854.png?ga=GA1.1.448448890.1721050418&semt=ais_hybrid"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Java
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://cdn-icons-png.freepik.com/256/5968/5968350.png?ga=GA1.1.448448890.1721050418&semt=ais_hybrid"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Python
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://cdn-icons-png.freepik.com/256/5968/5968267.png?ga=GA1.1.448448890.1721050418&semt=ais_hybrid"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               HTML
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://cdn-icons-png.freepik.com/256/5968/5968242.png?ga=GA1.1.448448890.1721050418&semt=ais_hybrid"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               CSS
             </div>
           </div>
@@ -101,111 +101,111 @@ const Skills = () => {
         </h2>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5">
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://cdn-icons-png.freepik.com/256/15697/15697481.png?ga=GA1.1.448448890.1721050418&semt=ais_hybrid"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               React.js
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Express.js
             </div>
           </div>
 
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://camo.githubusercontent.com/77139e445a39e2e6587d0d5c9abc370550e18eb8a3159bb5519abc0369b26e80/68747470733a2f2f69636f6e2e69636570616e656c2e696f2f546563686e6f6c6f67792f7376672f4e6f64652e6a732e737667"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Node.js
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://camo.githubusercontent.com/0568e2de313626b2bd9b96f326941b012d45e9a4db1a23aa78bd8036207e57f8/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667"
               className="w-20 h-20 rounded-full"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Tailwind
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://user-images.githubusercontent.com/25181517/189715289-df3ee512-6eca-463f-a0f4-c10d94a06b2f.png"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Figma
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://user-images.githubusercontent.com/25181517/192108372-f71d70ac-7ae6-4c0d-8395-51d8870c2ef0.png"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Git
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://sarthak-portfolio.onrender.com/images%202/skills/github1.png"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               GitHub
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://camo.githubusercontent.com/825bc22c1c4819762657b53f6491051ddc41482012d6fd8ae7b6b53776bf334e/68747470733a2f2f696d672e69636f6e73382e636f6d2f636f6c6f722f3435322f66697265626173652e706e67"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Firebase
             </div>
           </div>
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://user-images.githubusercontent.com/25181517/192109061-e138ca71-337c-4019-8d42-4792fdaa7128.png"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               Postman
             </div>
           </div>
@@ -218,14 +218,14 @@ const Skills = () => {
         </h2>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5">
           <div
-            className="flex flex-col items-center bg-purple-600 p-5 rounded-lg justify-center transition transform hover:scale-105"
+            className="flex flex-col items-center bg-purple-600 dark:bg-purple-200 p-5 rounded-lg justify-center "
             data-aos="flip-left"
           >
             <img
               src="https://sarthak-portfolio.onrender.com/images%202/skills/mongodb.png"
               className="w-20 h-20"
             />
-            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold" data-aos="zoom-in" >
+            <div className="   p-[12px] rounded-lg shadow-xl w-full text-center mt-5 bg-white text-purple-800 text-xl font-semibold dark:bg-purple-600 dark:text-white" data-aos="zoom-in" >
               MongoDB
             </div>
           </div>
