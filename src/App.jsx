716700953f8c128f4ae691e4981263d59b08ad9c
riya-rjha <riyaranjanjha2006@gmail.com>
2025-01-08 19:49:48 +0530

    Reorder components in App.jsx for improved structure

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,8 @@ const App = () => (
         element={
           <>
             <Main />
-            <Experience />
             <Projects/>
+            <Experience />
             <Skills/>
             <Contact/>
             <Socials />
